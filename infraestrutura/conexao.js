ab054173d27868097b08cdd8f9721b25480c14a5
Danillo <danillo.yamagush@gmail.com>
2021-08-26 11:44:11 -0300

    Ajuste de Data e mensagens de Erros.

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2')
 
 const conexao = mysql.createConnection({
-    host: '192.168.0.2',
-    port: 3306,
+    host: '192.168.0.8',
+    port: 3308,
     user: 'dancesar',
     password: '05041979',
     database: 'agendamento-petshop'
